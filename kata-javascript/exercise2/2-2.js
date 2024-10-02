20a77fa06ec6f8db11bec057a623a0db62065185
Adriel Gonzalo Barrientos Mercado <122940027+Adrielgbm@users.noreply.github.com>
2024-10-02 15:56:51 -0300

    add unit2-exercise2

---
+++ kata-javascript/exercise2/2-2.js
@@ -0,0 +1,4 @@
+let string = prompt("Por favor, ingresa varias palabras separadas por comas:");
+let arreglo = string.split(",");
+
+console.log(arreglo);
---
+++ kata-javascript/exercise2/2-3.js
@@ -0,0 +1,7 @@
+let numeros = [10, 40, 30, 20, 15, 5];
+
+numeros.sort((a, b) => a - b);
+
+console.log("Arreglo ordenado de menor a mayor:", numeros);
+console.log("Número menor:", numeros[0]);
+console.log("Número mayor:", numeros[numeros.length - 1]);
