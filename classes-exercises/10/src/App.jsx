523e6659431b46d9cb2bc41e6eb57b3ca3ef0b02
Adriel Gonzalo Barrientos Mercado <122940027+Adrielgbm@users.noreply.github.com>
2025-02-11 21:33:00 -0300

    update title and implement counter functionality with limits in classes exercise 10

--- classes-exercises/10/src/App.jsx
+++ classes-exercises/10/src/App.jsx
@@ -1,10 +1,41 @@
-import Main from "./components/organisms/Main";
+// import Main from "./components/organisms/Main";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState(0);
+  const [message, setMessage] = useState("");
+
+  const addPeople = () => {
+    if (people >= 10) {
+      setMessage("¡No puedes tener más de 10 personas!");
+      return;
+    }
+    setMessage("");
+    setPeople(people + 1);
+  };
+
+  const subtractPeople = () => {
+    if (people <= 0) {
+      setMessage("¡No puedes tener personas negativas!");
+      return;
+    }
+    setMessage("");
+    setPeople(people - 1);
+  };
+
   return (
     <>
-      <Main></Main>
+      <div className="container">
+        <h1 className="title">Contador: {people}</h1>
+        <button className="button" onClick={addPeople}>
+          +
+        </button>
+        <button className="button" onClick={subtractPeople}>
+          -
+        </button>
+      </div>
+      <p>{message}</p>
     </>
   );
 }
