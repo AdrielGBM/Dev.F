1602682002cf5a0501e63175b865dcdc1749d009
Adriel Gonzalo Barrientos Mercado <122940027+Adrielgbm@users.noreply.github.com>
2025-02-23 15:55:07 -0300

    add Pokémon modal section and fetch function in unit4-final-project

--- kata-javascript-avanzado/final-project/src/app.js
+++ kata-javascript-avanzado/final-project/src/app.js
@@ -1,3 +1,21 @@
-async function main() {}
+async function main() {
+  const pokemons = await getPokemons();
+  console.log(pokemons);
+}
+
+async function getPokemons() {
+  const URL =
+    "https://storage.googleapis.com/campus-cvs/00000000000-images-lectures/pokemons.json"; // API no disponible
+
+  const response = await fetch(URL, {
+    mode: "no-cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+  const data = await response.json();
+
+  return data;
+}
 
 document.addEventListener("DOMContentLoaded", main);
